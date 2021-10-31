bea01ac99cd27b58eb34e4c30dd7c56bb7c2c7cd
codThing <hensenhit@gmail.com>
2021-10-31 22:07:21 +0800

    1.4

--- 01bookstatic/book/src/main/java/web/RegistServlet.java
+++ 01bookstatic/book/src/main/java/web/RegistServlet.java
@@ -1,4 +1,14 @@
 package web;
 
-public class RegistServlet  {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RegistServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
 }
